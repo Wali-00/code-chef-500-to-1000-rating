801a76c3c1c8a13e663dc5f3e86ff422250093bd
Wali-00 <waliu4574@gmail.com>
2026-05-21 12:29:38 +0600

    rating 513

---
+++ 513.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin>>T;
+    while(T--){
+        int X;
+        cin>>X;
+        if(X+3<=10){
+            cout<<"YES"<<endl;
+        }
+    
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+    
+    
+
+}
