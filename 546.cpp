eff666e3b1cbf00f8038115d143d80b0b451f36e
Wali-00 <waliu4574@gmail.com>
2026-05-22 22:04:49 +0600

    rating 546

---
+++ 546.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	while(T--){
+	    int N,X;
+	    cin>>N>>X;
+	    double p=(N*X)/4.0;
+	    cout<<ceil(p)<<endl;
+	}
+}
