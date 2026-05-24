a61f4176870c4c679c79e9d8b43cb34871e3f460
Wali-00 <waliu4574@gmail.com>
2026-05-24 13:47:05 +0600

    rating 573

---
+++ 573.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int A,B,C,X;
+    cin>>A>>B>>C>>X;
+    if(A==X || B==X || C==X){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+}
