e4e4fb87df109f44c85f942ffea91117b2195312
Wali-00 <waliu4574@gmail.com>
2026-05-24 10:59:30 +0600

    rating 563

---
+++ 563.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+        int T;
+        cin>>T;
+        while(T--){
+            int N;
+            cin>>N;
+            int sell= 50*N;
+            cout<<sell-(sell*20/100)-(sell*20/100)-(sell*30/100)<<endl;
+        }
+
+}
