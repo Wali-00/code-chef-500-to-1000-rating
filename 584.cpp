34e33c06ebf63731f3c7e8d52ff5609b280efa00
Wali-00 <waliu4574@gmail.com>
2026-05-25 23:35:26 +0600

    rating 584

---
+++ 584.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	while(T--){
+	    int A,B,C;
+	    cin>>A>>B>>C;
+	    if(B>=A && B>=C){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+
+}
