8cc185f86e788f2c7fca5f5dfc9f78f9c392d7f1
Wali-00 <waliu4574@gmail.com>
2026-05-25 23:47:01 +0600

    rating 585

---
+++ 585.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	while(T--){
+	    int X;
+	    cin>>X;
+	    if(X<=50){
+	        cout<<"LEFT"<<endl;
+	    }
+	    else{
+	        cout<<"RIGHT"<<endl;
+	    }
+	}
+
+}
