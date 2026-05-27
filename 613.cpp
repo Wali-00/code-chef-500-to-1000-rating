501098c661c8f642bbc1f7dc41a2f5d53d476fc9
Wali-00 <waliu4574@gmail.com>
2026-05-27 22:40:02 +0600

    rating 613

---
+++ 613.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin>>T;
+	while(T--){
+	    int N,X;
+	    cin>>N>>X;
+	    if(N<=X && X%N==0){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+
+}
